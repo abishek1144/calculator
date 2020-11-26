ab170405b3d87bdce379c5f04e96293d057a59b0
abishek1144 <abishek4std@gmail.com>
2020-11-26 20:08:45 +0530

    added subtraction

--- scripts.js
+++ scripts.js
@@ -8,9 +8,9 @@ cod.addEventListener("submit",function(event){
         alert("Please enter the values")
     }
     else{
-        x=parseFloat(x.value);
-        y=parseFloat(y.value);
-        var result=(x/y)*100;
+        var a=parseFloat(x.value);
+        var b=parseFloat(y.value);
+        var result=(a/b)*100;
         resultfield.innerText=result;
         event.preventDefault();
     }
@@ -19,20 +19,38 @@ cod.addEventListener("submit",function(event){
 
 var x1=document.getElementById("num3");
 var y1=document.getElementById("num4");
-var cod1=document.getElementById("abi1");
 var resultfield1=document.getElementById("ans1");
+var bn2=document.getElementById("btn2");
+var bn3=document.getElementById("btn3");
 
-cod1.addEventListener("submit",function(event){
-    if(!x1.value || !y1.value){
-        alert("Please enter the values")
-    }
-    else{
-        x1=parseFloat(x1.value);
-        y1=parseFloat(y1.value);
-        var result1=x1+y1;
-        resultfield1.innerText=result1;
-        event.preventDefault();
-    }
-    
-})
+bn2.addEventListener('click',function(event){
+  if(!x1.value || !y1.value){
+    alert("Please enter values in the field");
+  }
+  else{
+    var a=parseFloat(x1.value);
+    var b=parseFloat(y1.value);
+    var result=a+b;
+
+    resultfield1.innerHTML=result;
+event.preventDefault();
+  }
+
+});
+
+bn3.addEventListener('click',function(event){
+  if(!x1.value || !y1.value){
+    alert("Please enter values in the field");
+  }
+  else{
+    var a=parseFloat(x1.value);
+    var b=parseFloat(y1.value);
+    var result=a-b;
+
+    resultfield1.innerHTML=result;
+event.preventDefault();
+  }
+
+});
+
 
