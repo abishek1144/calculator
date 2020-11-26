d82ffd57192adc899a89cd66ee4bf61b3debb735
abishek1144 <abishek4std@gmail.com>
2020-11-26 20:37:06 +0530

    added all operations

--- scripts.js
+++ scripts.js
@@ -22,6 +22,10 @@ var y1=document.getElementById("num4");
 var resultfield1=document.getElementById("ans1");
 var bn2=document.getElementById("btn2");
 var bn3=document.getElementById("btn3");
+var bn4=document.getElementById("btn4");
+var bn5=document.getElementById("btn5");
+var bn6=document.getElementById("btn6");
+var bn7=document.getElementById("btn7");
 
 bn2.addEventListener('click',function(event){
   if(!x1.value || !y1.value){
@@ -53,4 +57,63 @@ event.preventDefault();
 
 });
 
+bn4.addEventListener('click',function(event){
+  if(!x1.value || !y1.value){
+    alert("Please enter values in the field");
+  }
+  else{
+    var a=parseFloat(x1.value);
+    var b=parseFloat(y1.value);
+    var result=a*b;
+
+    resultfield1.innerHTML=result;
+event.preventDefault();
+  }
+
+});
+
+bn5.addEventListener('click',function(event){
+  if(!x1.value || !y1.value){
+    alert("Please enter values in the field");
+  }
+  else{
+    var a=parseFloat(x1.value);
+    var b=parseFloat(y1.value);
+    var result=a/b;
+
+    resultfield1.innerHTML=result;
+event.preventDefault();
+  }
+
+});
+
+bn6.addEventListener('click',function(event){
+  if(!x1.value || !y1.value){
+    alert("Please enter values in the field");
+  }
+  else{
+    var a=parseFloat(x1.value);
+    var b=parseFloat(y1.value);
+    var result=a%b;
+
+    resultfield1.innerHTML=result;
+event.preventDefault();
+  }
+
+});
+
+bn7.addEventListener('click',function(event){
+  if(!x1.value || !y1.value){
+    alert("Please enter values in the field");
+  }
+  else{
+    var a=parseFloat(x1.value);
+    var b=parseFloat(y1.value);
+    var result=a**b;
+
+    resultfield1.innerHTML=result;
+event.preventDefault();
+  }
+
+});
 
